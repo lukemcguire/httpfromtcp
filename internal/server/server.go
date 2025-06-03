da4aa50270dfa7bdf82a8d6385e651a0a31c363c
Luke McGuire <luke.mcguire@gmail.com>
2025-06-02 21:03:17 -0700

    feat: basic http server

---
+++ internal/server/server.go
@@ -0,0 +1,62 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync/atomic"
+)
+
+type Server struct {
+	listener net.Listener
+	closed   atomic.Bool
+}
+
+func Serve(port int) (*Server, error) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return nil, fmt.Errorf("error creating tcp listener: %w", err)
+	}
+
+	server := &Server{
+		listener: listener,
+	}
+
+	go server.listen()
+
+	return server, nil
+}
+
+func (s *Server) Close() error {
+	s.closed.Store(true)
+	if s.listener != nil {
+		s.listener.Close()
+	}
+	return nil
+}
+
+func (s *Server) listen() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			if s.closed.Load() {
+				return
+			}
+			log.Printf("error accepting connection: %s", err)
+			continue
+		}
+		go s.handle(conn)
+	}
+}
+
+func (s *Server) handle(conn net.Conn) {
+	defer conn.Close()
+	response := "HTTP/1.1 200 OK\r\n" +
+		"Content-Type: text/plain\r\n" +
+		"\r\n" +
+		"Hello World!\n"
+	_, err := conn.Write([]byte(response))
+	if err != nil {
+		log.Printf("error writing to connection: %s", err)
+	}
+}
